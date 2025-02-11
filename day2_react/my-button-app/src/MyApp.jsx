cac1cde5d35de4326af100be9cccfed35440502c
pratham-12-lab <prathamlgaikwad468@gmail.com>
2025-02-11 15:20:39 +0530

    Java Scrpt notes is updates

---
+++ day2_react/my-button-app/src/MyApp.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+ function MyApp () {
+    return (
+        <>
+        <h2>My first react app</h2>
+        </>
+    )
+}
+
+export default MyApp;
--- day2_react/my-button-app/src/main.jsx
+++ day2_react/my-button-app/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import MyApp from './MyApp.jsx'
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <MyApp />
     <App />
   </StrictMode>,
 )
